cfb6a928db18675411b0ed7d3090a209ec7ea8f7
No1Austin <amadiaustin@rocketmail.com>
2025-09-29 16:44:21 -0400

    Initial commit (clean, no large files)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserList from "./UserList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Checkpoint: API Users</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function UserList() {
+  const [listOfUser, setListOfUser] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setListOfUser(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {listOfUser.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> – {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
